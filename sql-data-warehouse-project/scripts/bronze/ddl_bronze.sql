0a7833c719e2fdf1502cab2e06b7859822933402
nikhilnagar503 <144032973+nikhilnagar503@users.noreply.github.com>
2025-04-14 13:54:58 +0530

    Update ddl_bronze.sql

--- sql-data-warehouse-project/scripts/bronze/ddl_bronze.sql
+++ sql-data-warehouse-project/scripts/bronze/ddl_bronze.sql
@@ -5,47 +5,45 @@ DDL Script: Create Bronze Tables
 Script Purpose:
     This script creates tables in the 'bronze' schema, dropping existing tables 
     if they already exist.
-	  Run this script to re-define the DDL structure of 'bronze' Tables
+    Run this script to re-define the DDL structure of 'bronze' Tables
 ===============================================================================
 */
 
-IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
-    DROP TABLE bronze.crm_cust_info;
-GO
+-- Drop the 'bronze.crm_cust_info' table if it exists
+DROP TABLE IF EXISTS bronze.crm_cust_info;
 
+-- Create the 'bronze.crm_cust_info' table
 CREATE TABLE bronze.crm_cust_info (
     cst_id              INT,
-    cst_key             NVARCHAR(50),
-    cst_firstname       NVARCHAR(50),
-    cst_lastname        NVARCHAR(50),
-    cst_marital_status  NVARCHAR(50),
-    cst_gndr            NVARCHAR(50),
+    cst_key             VARCHAR(50),
+    cst_firstname       VARCHAR(50),
+    cst_lastname        VARCHAR(50),
+    cst_marital_status  VARCHAR(50),
+    cst_gndr            VARCHAR(50),
     cst_create_date     DATE
 );
-GO
 
-IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
-    DROP TABLE bronze.crm_prd_info;
-GO
+-- Drop the 'bronze.crm_prd_info' table if it exists
+DROP TABLE IF EXISTS bronze.crm_prd_info;
 
+-- Create the 'bronze.crm_prd_info' table
 CREATE TABLE bronze.crm_prd_info (
     prd_id       INT,
-    prd_key      NVARCHAR(50),
-    prd_nm       NVARCHAR(50),
+    prd_key      VARCHAR(50),
+    prd_nm       VARCHAR(50),
     prd_cost     INT,
-    prd_line     NVARCHAR(50),
+    prd_line     VARCHAR(50),
     prd_start_dt DATETIME,
     prd_end_dt   DATETIME
 );
-GO
 
-IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
-    DROP TABLE bronze.crm_sales_details;
-GO
+-- Drop the 'bronze.crm_sales_details' table if it exists
+DROP TABLE IF EXISTS bronze.crm_sales_details;
 
+-- Create the 'bronze.crm_sales_details' table
 CREATE TABLE bronze.crm_sales_details (
-    sls_ord_num  NVARCHAR(50),
-    sls_prd_key  NVARCHAR(50),
+    sls_ord_num  VARCHAR(50),
+    sls_prd_key  VARCHAR(50),
     sls_cust_id  INT,
     sls_order_dt INT,
     sls_ship_dt  INT,
@@ -54,37 +52,33 @@ CREATE TABLE bronze.crm_sales_details (
     sls_quantity INT,
     sls_price    INT
 );
-GO
 
-IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
-    DROP TABLE bronze.erp_loc_a101;
-GO
+-- Drop the 'bronze.erp_loc_a101' table if it exists
+DROP TABLE IF EXISTS bronze.erp_loc_a101;
 
+-- Create the 'bronze.erp_loc_a101' table
 CREATE TABLE bronze.erp_loc_a101 (
-    cid    NVARCHAR(50),
-    cntry  NVARCHAR(50)
+    cid    VARCHAR(50),
+    cntry  VARCHAR(50)
 );
-GO
 
-IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
-    DROP TABLE bronze.erp_cust_az12;
-GO
+-- Drop the 'bronze.erp_cust_az12' table if it exists
+DROP TABLE IF EXISTS bronze.erp_cust_az12;
 
+-- Create the 'bronze.erp_cust_az12' table
 CREATE TABLE bronze.erp_cust_az12 (
-    cid    NVARCHAR(50),
+    cid    VARCHAR(50),
     bdate  DATE,
-    gen    NVARCHAR(50)
+    gen    VARCHAR(50)
 );
-GO
 
-IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
-    DROP TABLE bronze.erp_px_cat_g1v2;
-GO
+-- Drop the 'bronze.erp_px_cat_g1v2' table if it exists
+DROP TABLE IF EXISTS bronze.erp_px_cat_g1v2;
 
+-- Create the 'bronze.erp_px_cat_g1v2' table
 CREATE TABLE bronze.erp_px_cat_g1v2 (
-    id           NVARCHAR(50),
-    cat          NVARCHAR(50),
-    subcat       NVARCHAR(50),
-    maintenance  NVARCHAR(50)
+    id           VARCHAR(50),
+    cat          VARCHAR(50),
+    subcat       VARCHAR(50),
+    maintenance  VARCHAR(50)
 );
-GO
